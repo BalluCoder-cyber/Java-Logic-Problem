14c60e1b96f9c79dc67eb664bb11be4d8e0f79c4
BalluCoder-cyber <balwantahirwar70@gmail.com>
2026-04-14 21:57:53 +0530

    prime number

---
+++ src/PrimeNumber.java
@@ -0,0 +1,16 @@
+public class PrimeNumber {
+    public static boolean primeValue(int n){
+        if(n<=1) return true;
+        for(int i =2; i<n; i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+
+        System.out.println(primeValue(9));
+
+    }
+}
