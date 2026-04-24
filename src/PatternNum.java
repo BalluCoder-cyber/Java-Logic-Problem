5eab6c35d8b89590a6b03f3276e7561cff8dda78
BalluCoder-cyber <balwantahirwar70@gmail.com>
2026-04-24 15:35:41 +0530

    pattern number

---
+++ src/PatternNum.java
@@ -0,0 +1,10 @@
+public class PatternNum {
+    public static void main(String[]args){
+        for (int i = 1; i<=5;i++){
+            for (int j = 5; j>= 1; j--){
+                System.out.print(j+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
