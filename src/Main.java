6b11cbdf7934a9efd05ea19b2e087c91d14bde28
BalluCoder-cyber <balwantahirwar70@gmail.com>
2026-04-26 09:26:21 +0530

    Divisible by 7

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter your num");
+        int num = s.nextInt();
+        for(int i =2;i<num;i++){
+            if(num%i == 0){
+                System.out.println("prime");
+
+            }else{
+                System.out.println("not prime");
+            }
+        }
+
+    }
+
 
 }
